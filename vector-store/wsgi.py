28b7dbbc66db6bc284bf5a7c5c5ed419b6b94b7f
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-10-26 03:34:28 -0700

    update

---
+++ vector-store/wsgi.py
@@ -0,0 +1,10 @@
+"""
+WSGI configuration for Gunicorn deployment
+"""
+import os
+from app import app
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
